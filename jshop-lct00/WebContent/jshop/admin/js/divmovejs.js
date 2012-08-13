9f3e2f19c929548a8ddad2ab1b1ed44d86aa7d77
chen da <sdywcd@gmail.com>
2012-08-13 14:12:38 +0800

    1

--- jshop-lct00/WebContent/jshop/admin/js/divmovejs.js
+++
@@ -1,4 +0,0 @@
-function IFrameResize(){
- 	 var obj = parent.document.getElementById("frm_main"); 
- 	 obj.height = this.document.body.scrollHeight+100; 
- 	}
--- jshop-lct00/WebContent/jshop/admin/resources/scripts/smooth.autocomplete.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $("input#autocomplete").autocomplete({
-        source: ["c++", "java", "php", "coldfusion", "javascript", "asp", "ruby"]
-    });
-});
